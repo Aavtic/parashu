ac9e592740239dc7c8c0d1943860fd1018e81379
Aavtic <aadishm79@gmail.com>
2024-08-19 00:13:35 +0530

    Added Styles, Events parser

--- src/ass_parser/mod.rs
+++ src/ass_parser/mod.rs
@@ -1,6 +1,7 @@
 use hex_color::HexColor;
 use std::{fs, io::Read};
 use std::io::{Seek, Write};
+use std::ops::Deref;
 
 
 const SCRIPT_HEADER:&str = "[Script Info]";
@@ -16,7 +17,7 @@ const EVENT_HEAD:&str = "Dialogue: ";
 
 
 #[derive(Debug, Clone)]
-struct ScriptInfo {
+pub struct ScriptInfo {
     scripttype: Option<String>,
     playresx: Option<String>,
     playresy: Option<String>,
@@ -37,30 +38,35 @@ impl ScriptInfo {
 }
 
 impl ScriptInfo {
-    fn set_scripttype(mut self, value: String) -> Self {
+    pub fn set_scripttype(&mut self, value: String) -> &mut Self {
 		self.scripttype = Some(value);
 		self
 	}
-    fn set_playresx(mut self, value: String) -> Self {
+    pub fn set_playresx(&mut self, value: String) -> &mut Self {
 		self.playresx = Some(value);
 		self
 	}
-    fn set_playresy(mut self, value: String) -> Self {
+    pub fn set_playresy(&mut self, value: String) -> &mut Self {
 		self.playresy = Some(value);
 		self
 	}
-    fn set_scaledborderandshadow(mut self, value: String) -> Self {
+    pub fn set_scaledborderandshadow(&mut self, value: String) -> &mut Self {
 		self.scaledborderandshadow = Some(value);
 		self
 	}
-    fn set_ycbcr_matrix(mut self, value: String) -> Self {
+    pub fn set_ycbcr_matrix(&mut self, value: String) -> &mut Self {
 		self.ycbcr_matrix = Some(value);
 		self
 	}
 }
 
+impl ScriptInfo {
+    fn into(&self) {
+    }
+}
+
 #[derive(Debug, Clone)]
-struct V4Format {
+pub struct V4Format {
     name: Option<String>,
     fontname: Option<String>,
     fontsize: Option<String>,
@@ -87,7 +93,7 @@ struct V4Format {
 }
 
 impl V4Format {
-    fn new() -> Self {
+    fn new() -> V4Format {
         Self {
             name: None,
             fontname: None,
@@ -116,97 +122,183 @@ impl V4Format {
     }
 }
 
+impl Default for V4Format {
+    fn default() -> V4Format {
+        V4Format {
+        name: Some("Default".to_string()),
+        fontname: Some("Arial".to_string()),
+        fontsize: Some("16".to_string()),
+        primarycolour: Some("&Hffffff".to_string()),
+        secondarycolour: Some("&Hffffff".to_string()),
+        outlinecolour:Some("&H0".to_string()),
+        backcolour: Some("&H0".to_string()),
+        bold: Some("0".to_string()),
+        italic: Some("0".to_string()),
+        underline: Some("0".to_string()),
+        strikeout: Some("0".to_string()),
+        scalex: Some("100".to_string()),
+        scaley: Some("100".to_string()),
+        spacing: Some("0".to_string()),
+        angle:Some("0".to_string()),
+        borderstyle:Some("1".to_string()),
+        outline: Some("1".to_string()),
+        shadow: Some("0".to_string()),
+        alignment: Some("2".to_string()),
+        marginl:Some("10".to_string()),
+        marginr: Some("10".to_string()),
+        marginv: Some("10".to_string()),
+        encoding: Some("1".to_string()),
+        }
+    }
+}
+
+impl V4Format {
+    pub fn set_v4(&mut self, v4: V4Format) -> &mut V4Format {
+        *self = v4;
+        self
+    }
+    fn get_array(&self) -> [&Option<String>; 23] {
+        [
+            &self.name,
+            &self.fontname,
+            &self.fontsize,
+            &self.primarycolour,
+            &self.secondarycolour,
+            &self.outlinecolour,
+            &self.backcolour,
+            &self.bold,
+            &self.italic,
+            &self.underline,
+            &self.strikeout,
+            &self.scalex,
+            &self.scaley,
+            &self.spacing,
+            &self.angle,
+            &self.borderstyle,
+            &self.outline,
+            &self.shadow,
+            &self.alignment,
+            &self.marginl,
+            &self.marginr,
+            &self.marginv,
+            &self.encoding,
+            ]
+    }
+}
+
 impl V4Format {
     // Ik this looks crazy. but what do?
-	fn set_name(mut self, value: String) -> Self {
+	pub fn set_name(&mut self,
+                    value: String) -> &mut Self{
         self.name = Some(value);
         self
 	}
-	fn set_fontname(mut self, value: String) -> Self {
+	pub fn set_fontname(&mut self,
+                        value: String) -> &mut Self{
         self.fontname = Some(value);
         self
 	}
-	fn set_fontsize(mut self, value: String) -> Self {
+	pub fn set_fontsize(&mut self,
+                        value: String) -> &mut Self{
         self.fontsize = Some(value);
         self
 	}
-	fn set_primarycolour(mut self, value: String) -> Self {
+	pub fn set_primarycolour(&mut self,
+                             value: String) -> &mut Self{
         self.primarycolour = Some(value);
         self
 	}
-	fn set_secondarycolour(mut self, value: String) -> Self {
+	pub fn set_secondarycolour(&mut self,
+                               value: String) -> &mut Self{
         self.secondarycolour = Some(value);
         self
 	}
-	fn set_outlinecolour(mut self, value: String) -> Self {
+	pub fn set_outlinecolour(&mut self,
+                             value: String) -> &mut Self{
         self.outlinecolour = Some(value);
         self
 	}
-	fn set_backcolour(mut self, value: String) -> Self {
+	pub fn set_backcolour(&mut self,
+                          value: String) -> &mut Self{
         self.backcolour = Some(value);
         self
 	}
-	fn set_bold(mut self, value: String) -> Self {
+	pub fn set_bold(&mut self,
+                    value: String) -> &mut Self{
         self.bold = Some(value);
         self
 	}
-	fn set_italic(mut self, value: String) -> Self {
+	pub fn set_italic(&mut self,
+                      value: String) -> &mut Self{
         self.italic = Some(value);
         self
 	}
-	fn set_underline(mut self, value: String) -> Self {
+	pub fn set_underline(&mut self,
+                         value: String) -> &mut Self{
         self.underline = Some(value);
         self
 	}
-	fn set_strikeout(mut self, value: String) -> Self {
+	pub fn set_strikeout(&mut self,
+                         value: String) -> &mut Self{
         self.strikeout = Some(value);
         self
 	}
-	fn set_scalex(mut self, value: String) -> Self {
+	pub fn set_scalex(&mut self,
+                      value: String) -> &mut Self{
         self.scalex = Some(value);
         self
 	}
-	fn set_scaley(mut self, value: String) -> Self {
+	pub fn set_scaley(&mut self,
+                      value: String) -> &mut Self{
         self.scaley = Some(value);
         self
 	}
-	fn set_spacing(mut self, value: String) -> Self {
+	pub fn set_spacing(&mut self,
+                       value: String) -> &mut Self{
         self.spacing = Some(value);
         self
 	}
-	fn set_angle(mut self, value: String) -> Self {
+	pub fn set_angle(&mut self,
+                     value: String) -> &mut Self{
         self.angle = Some(value);
         self
 	}
-	fn set_borderstyle(mut self, value: String) -> Self {
+	pub fn set_borderstyle(&mut self,
+                           value: String) -> &mut Self{
         self.borderstyle = Some(value);
         self
 	}
-	fn set_outline(mut self, value: String) -> Self {
+	pub fn set_outline(&mut self,
+                       value: String) -> &mut Self{
         self.outline = Some(value);
         self
 	}
-	fn set_shadow(mut self, value: String) -> Self {
+	pub fn set_shadow(&mut self,
+                      value: String) -> &mut Self{
         self.shadow = Some(value);
         self
 	}
-	fn set_alignment(mut self, value: String) -> Self {
+	pub fn set_alignment(&mut self,
+                         value: String) -> &mut Self{
         self.alignment = Some(value);
         self
 	}
-	fn set_marginl(mut self, value: String) -> Self {
+	pub fn set_marginl(&mut self,
+                       value: String) -> &mut Self{
         self.marginl = Some(value);
         self
 	}
-	fn set_marginr(mut self, value: String) -> Self {
+	pub fn set_marginr(&mut self,
+                       value: String) -> &mut Self{
         self.marginr = Some(value);
         self
 	}
-	fn set_marginv(mut self, value: String) -> Self {
+	pub fn set_marginv(&mut self,
+                       value: String) -> &mut Self{
         self.marginv = Some(value);
         self
 	}
-	fn set_encoding(mut self, value: String) -> Self {
+	fn set_encoding(&mut self, value: String) -> &mut Self{
         self.encoding = Some(value);
         self
 	}
@@ -262,6 +354,25 @@ impl Dialogue {
 }
 
 impl Dialogue {
+    fn to_string(&self) -> String {
+        let mut dialogue_string = String::new();
+        dialogue_string.push_str(EVENT_HEAD);
+        dialogue_string.push_str(&(self.event.layer.as_ref().unwrap().to_owned() + ","));
+        dialogue_string.push_str(&(self.event.start.as_ref().unwrap().to_owned() + ","));
+        dialogue_string.push_str(&(self.event.end.as_ref().unwrap().to_owned() + ","));
+        dialogue_string.push_str(&(self.event.style.as_ref().unwrap().to_owned() + ","));
+        dialogue_string.push_str(&(self.event.name.as_ref().unwrap().to_owned() + ","));
+        dialogue_string.push_str(&(self.event.marginl.as_ref().unwrap().to_owned() + ","));
+        dialogue_string.push_str(&(self.event.marginr.as_ref().unwrap().to_owned() + ","));
+        dialogue_string.push_str(&(self.event.marginv.as_ref().unwrap().to_owned() + ","));
+        dialogue_string.push_str(&(self.event.effect.as_ref().unwrap().to_owned() + ","));
+        dialogue_string.push_str(&(self.event.text.as_ref().unwrap().to_owned() + ",\n"));
+
+        return dialogue_string;
+    }
+}
+
+impl Dialogue {
     fn set_layer(mut self, value: String) -> Self {
 		self.event.layer = Some(value);
 		self
@@ -307,67 +418,132 @@ impl Dialogue {
 pub struct AssFileOptions{}
 
 #[derive(Clone)]
-struct Components {
-    script: ScriptInfo,
-    v4: V4Format,
-    events: Events,
+pub struct Components {
+    pub script: ScriptInfo,
+    pub v4: V4Format,
+    pub events: Events,
 }
 
-pub struct AssFile{
-    _ass_file: String,
+struct MutableComponent {
+    master: AssFile,
     components: Components,
+}
+
+#[derive(Clone)]
+pub struct AssFile{
+    ass_file: String,
+    pub components: Components,
+}
+
+impl Deref for AssFile {
+    type Target = Components;
+
+    fn deref(&self) -> &Self::Target {
+        &self.components
+    }
 }
 
 struct Parser; 
 impl Parser {
     fn new() -> Parser {
-        Parser {}
+        Parser
     }
 
-    fn combine_components(&self, components: &Components, contents: String)  {
+    fn combine_components(&self, components: &Components, contents: String) -> String {
         let components = components.clone();
         let script = components.script;
-        let _v4 = components.v4;
-        let _events = components.events;
+        let v4 = components.v4;
+        let events = components.events;
         let lines:Vec<&str> = contents.split("\n").collect();
         let  script_lines = self.get_info(&lines, SCRIPT_HEADER);
         let mut _v4_lines = &self.get_info(&lines, V4_HEADER);
         let mut _events_lines = &self.get_info(&lines, EVENTS_HEADER);
 
-        let _ = &self.plug_script(script_lines, script);
+        let script_data  = &self.plug_script(script_lines, script);
+        let v4_data = &self.plug_v4(v4);
+        let event_data = &self.plug_events(events);
+        let total_data = format!("{}\n\n{}\n\n{}", script_data, v4_data, event_data);
+
+        return total_data;
     }
-    
-    fn plug_script(&self, script_lines: Vec<String>, scriptinfo: ScriptInfo) {
+
+    fn plug_script(&self, script_lines: Vec<String>, scriptinfo: ScriptInfo) -> String {
         let mut new_lines = script_lines.clone();
+        let mut total_lines = String::new();
         let script_type = scriptinfo.scripttype.unwrap();
         let playresx = scriptinfo.playresx.unwrap();
         let playresy = scriptinfo.playresy.unwrap();
         let scaledborderandshadow = scriptinfo.scaledborderandshadow.unwrap();
         let ycbcr_matrix = scriptinfo.ycbcr_matrix.unwrap();
 
+
         for (i, line) in script_lines.iter().enumerate() {
             if line.starts_with(SCRIPT_TYPE) {
-                new_lines[i] = line[..SCRIPT_TYPE.len()].to_owned() + &script_type;
+                new_lines[i] = line[..SCRIPT_TYPE.len()].to_owned() + &script_type + "\n";
                 continue
             } else if line.starts_with(SCRIPT_PLAYRESX){
-                new_lines[i] = line[..SCRIPT_PLAYRESX.len()].to_owned() + &playresx;
+                new_lines[i] = line[..SCRIPT_PLAYRESX.len()].to_owned() + &playresx + "\n";
                 continue
             } else if line.starts_with(SCRIPT_PLAYRESY){
-                new_lines[i] = line[..SCRIPT_PLAYRESY.len()].to_owned() + &playresy;
+                new_lines[i] = line[..SCRIPT_PLAYRESY.len()].to_owned() + &playresy + "\n";
                 continue;
             } else if line.starts_with(SCRIPT_SCALEDBORDERANDSHADOW){
-                new_lines[i] = line[..SCRIPT_SCALEDBORDERANDSHADOW.len()].to_owned() + &scaledborderandshadow;
+                new_lines[i] = line[..SCRIPT_SCALEDBORDERANDSHADOW.len()].to_owned() + &scaledborderandshadow + "\n";
                 continue;
             } else if line.starts_with(SCRIPT_YCBCR_MATRIX){
-                new_lines[i] = line[..SCRIPT_YCBCR_MATRIX.len()].to_owned() + &ycbcr_matrix;
+                new_lines[i] = line[..SCRIPT_YCBCR_MATRIX.len()].to_owned() + &ycbcr_matrix + "\n";
                 continue;
             }
         }
 
         for line in new_lines {
-            println!("{}", line)
+            total_lines.push_str(line.as_str());
         }
 
+        return total_lines;
+    }
+
+    fn plug_v4(&self, v4_info: V4Format) -> String {
+        let array = v4_info.get_array();
+        let mut values = Vec::new();
+        let mut v4_lines = Vec::new();
+        let mut total_v4 = String::new();
+        v4_lines.push("[V4+ Styles]\n".to_string());
+        v4_lines.push("Format: Name, Fontname, Fontsize, PrimaryColour, SecondaryColour, OutlineColour, BackColour, Bold, Italic, Underline, StrikeOut, ScaleX, ScaleY, Spacing, Angle, BorderStyle, Outline, Shadow, Alignment, MarginL, MarginR, MarginV, Encoding\n".to_string());
+        v4_lines.push(V4_STYLE_HEAD.to_string());
+
+        for value in array {
+            let style_val = value.clone().unwrap();
+            values.push(style_val + ",");
+        }
+
+        values.push("\n".to_string());
+
+        println!("{:?}", values);
+        v4_lines.append(&mut values); 
+
+        for line in v4_lines {
+            total_v4.push_str(line.as_str());
+        }
+        return total_v4;
+    }
+
+    fn plug_events(&self, event_info: Events) -> String {
+        let mut lines = Vec::new();
+        let mut total_events = String::new();
+        let dialogues = event_info.dialogues.dialogues;
+        lines.push(EVENTS_HEADER.to_string() + "\n");
+        lines.push("Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text".to_string() + "\n");
+
+        for dialogue in dialogues {
+            let dialogue_line = dialogue.to_string();
+            lines.push(dialogue_line);
+        }
+        for line in lines {
+            total_events.push_str(line.as_str());
+        }
+        
+        return total_events;
     }
 
     fn get_each_components(&self, file_contents: String) -> Components {
@@ -386,7 +562,7 @@ impl Parser {
             events,
         }
 
-    }
+.clone()    }
     fn parse_script(&self, script_lines: Vec<String>) -> Option<ScriptInfo> {
         let mut script_type: Option<String>= None;
         let mut script_playerresx: Option<String>= None;
@@ -418,14 +594,15 @@ impl Parser {
                  script_scaledborderandshadow,
                  script_ycbcr_matrix);
 
-        Some(
-            ScriptInfo::new().
+        let mut scriptinfo = ScriptInfo::new();
+        let script_info = scriptinfo.
             set_scripttype(script_type.unwrap()).
             set_playresx(script_playerresx.unwrap()).
             set_playresy(script_playerresy.unwrap()).
             set_scaledborderandshadow(script_scaledborderandshadow.unwrap()).
-            set_ycbcr_matrix(script_ycbcr_matrix.unwrap())
-        )
+            set_ycbcr_matrix(script_ycbcr_matrix.unwrap()).clone();
+
+        Some(script_info)
 }
     fn parse_event(&self, event_lines: Vec<String>) -> Option<Events>{
         // let mut events = Vec::new();
@@ -499,7 +676,7 @@ impl Parser {
                 set_marginl(values[19].to_string()).
                 set_marginr(values[20].to_string()).
                 set_marginv(values[22].to_string()).
-                set_encoding(values[22].to_string());
+                set_encoding(values[22].to_string()).clone();
 
             return Some(v4format);
         } else {
@@ -512,16 +689,18 @@ impl Parser {
     fn get_info(&self, lines: &Vec<&str>, header: &str) -> Vec<String> {
         let mut script_lines = Vec::new();
         let mut found_script_header = false;
-
         for line in lines {
             if *line == header{
                 found_script_header = true;
                 script_lines.push(line.to_string());
+                script_lines.push("\n; Script generated by Parashu\n".to_string());
                 continue
             }
             if found_script_header {
                 if line.starts_with('[') {
                     break;
+                } else if line.starts_with(';') {
+                    continue;
                 } else {
                     script_lines.push(line.to_string());
                 }
@@ -539,17 +718,22 @@ impl AssFile {
         let parser = Parser::new();
         let components = parser.get_each_components(file_contents);
         Self{
-            _ass_file: filename,
+            ass_file: filename,
             components,
         }
     }
 
-    fn save_file(&self, filename: &str) {
-        let parser = Parser::new();
-        let components = &self.components;
-        let contents = get_contents(filename).unwrap();
+}
 
-        parser.combine_components(components, contents);
+impl AssFile {
+    pub fn save_file(file_components: &AssFile, filename: &str) {
+        let parser = Parser::new();
+        let components = &file_components.components;
+        let contents = get_contents(&file_components.ass_file).unwrap();
+
+        let file_data = parser.combine_components(components, contents);
+        write_contents(filename, &file_data);
+        println!("File saved successfully");
     }
 }
 
@@ -557,7 +741,7 @@ impl AssFileOptions {
 }
 
 impl AssFileOptions{
-    fn _get_ass_color(color: HexColor) -> String {
+    pub fn _get_ass_color(color: HexColor) -> String {
         let red = color.r;
         let green = color.g;
         let blue = color.b;
@@ -568,7 +752,8 @@ impl AssFileOptions{
 
         let reversed_hex_color = format!("{}{}{}", blue_hex, green_hex, red_hex);
 
-        let mut ass_format_color = format!(r"\c&H{}&", reversed_hex_color);
+        // let mut ass_format_color = format!(r"\c&H{}&", reversed_hex_color);
+        let mut ass_format_color = format!(r"&H{}", reversed_hex_color);
         ass_format_color.push('}');
         ass_format_color = "{".to_owned() + &ass_format_color;
 
@@ -647,6 +832,11 @@ fn write_dialogues(filename: &str, dialogues: Vec<String>) {
     for line in dialogues {
         file.write(line.as_bytes()).unwrap();
     } 
+}
+
+fn write_contents(filename: &str, contents: &str) {
+    let mut file = fs::File::create(filename).unwrap();
+    file.write(contents.as_bytes()).unwrap();
 }
 
 fn get_contents(filename: &str) -> Result<String, std::io::Error>{
